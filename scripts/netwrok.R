4e112f4ddd87eb47fc5c01c12cea85c07fd35f22
jaspkaur <jasbadesha27@gmail.com>
2018-04-10 18:22:43 -0500

    minor changes

--- scripts/netwrok.R
+++ scripts/netwrok.R
@@ -17,13 +17,13 @@ source("scripts/pico_root_phyloseq_object_sintax.R")
 ##make root otu file
 ##"Remove taxa not seen more than 3 times in at least 5% of the samples. 
 #This protects against an OTU with small mean & trivially large C.V.
-#d.r.net = filter_taxa(d_r, function(x) sum(x > 1) > (0.05*length(x)), TRUE)
-d.r.net = merge_samples(d_r, "int")
+d.r.net = filter_taxa(d_r, function(x) sum(x > 1) > (0.05*length(x)), TRUE)
+d.r.net = merge_samples(d.r.net, "int")
 otu.r.net = data.frame(otu_table(d.r.net)) ##it should be non-normalized
 ###slelect OTUs first from which you want to build network, rows shud be samples
 #otu.r.net = otu.r.net[row.names(otu.r.net) %in% row.names(sim.kw.popsize),]
-taxa_names(d.r.net) = paste(gsub("o", "r", taxa_names(d.r.net)))
 colnames(otu.r.net) = paste(gsub("o", "r", colnames(otu.r.net)))
+taxa_names(d.r.net) = paste(gsub("o", "r", taxa_names(d.r.net)))
 
 d.s = subset_samples(d, Population == "PLF"|Population == "PLE"
                          |Population == "SCW"| Population == "SCE"
@@ -34,34 +34,38 @@ d.s.net = subset_samples(d.s, Source == "S")
 d.s.net = subset_samples(d.s.net, Month == "Feb"| Month == "Apr")
 d.s.net = prune_taxa(taxa_sums(d.s.net) >= 1, d.s.net)
 d.s.net
-d.s.net = filter_taxa(d.s.net, function(x) sum(x > 1) > (0.05*length(x)), TRUE)
+d.s.net = filter_taxa(d.s.net, function(x) sum(x > 2) > (0.05*length(x)), TRUE)
 d.s.net = merge_samples(d.s.net, "int")
 d.s.net
 otu.s.net = data.frame(otu_table(d.s.net)) ##it should no non-normalized
-taxa_names(d.s.net) = paste(gsub("o", "s", taxa_names(d.s.net)))
 colnames(otu.s.net) = paste(gsub("o", "s", colnames(otu.s.net)))
+taxa_names(d.s.net) = paste(gsub("o", "s", taxa_names(d.s.net)))
 
 d.net = merge_phyloseq(d.r.net, d.s.net)
 
 ###Sparcc
-spar = sparcc(data.frame(otu_table(d.net)))
-sparcc.graph <- abs(spar$Cor) >= 0.3
-diag(sparcc.graph) <- 0
-library(Matrix)
-sparcc.graph <- Matrix(sparcc.graph, sparse=TRUE)
-ig.sparcc <- adj2igraph(sparcc.graph)
-write.graph(ig.sparcc,file="ig.sparcc.txt",format="ncol") 
+##merge root and soil otu files together
 
-vsize <- rowMeans(clr(data.frame(otu_table(d.net)), 1))+6
-am.coord <- layout.fruchterman.reingold(ig.mb)
+net = cbind(otu.s.net, otu.r.net, by = "row.names")
+net = net[,-ncol(net)]
 
-plot(ig.sparcc, vertex.label=NA, main="sparcc")
+net = t(net)
+write.csv(net, file = "results/net.csv")
+#convert csv to .txt file
+system("python scripts/yonatanf-sparcc-05f4d3f31d77/SparCC.py results/net.txt -i 10 --cor_file=results/sparcc_corr.txt > results/sparcc.log")
+spec.cor = read.delim("results/sparcc_corr.txt", sep = "\t")
+row.names(spec.cor) = spec.cor[,1]
+spec.cor = melt(spec.cor)
+spec.cor.t.sel = spec.cor[grepl("s", spec.cor$X), ] #target selection
+spec.cor.fin.sel = spec.cor.t.sel[grepl("r", spec.cor.t.sel$variable), ]
+#ind.col.sel = ind.col[ind.col$value > 0.1,]
+write.csv(spec.cor.fin.sel, file = "results/spec.cor.fin.sel.csv")
 
 ####SpiecEasi
 
 spiec.out=spiec.easi(d.net, method="mb",icov.select.params=list(rep.num=20))
 spiec.graph=adj2igraph(spiec.out$refit, vertex.attr=list(name=taxa_names(d.net)))
-write.graph(spiec.graph,file="spieceasi.ncol.txt",format="ncol") 
+write.graph(spiec.graph,file="results/spieceasi.ncol.txt",format="ncol") 
 plot_network(spiec.graph, d.net, type='taxa', color = "Family", label="value")
 
 clusters=cluster_fast_greedy(spiec.graph)
